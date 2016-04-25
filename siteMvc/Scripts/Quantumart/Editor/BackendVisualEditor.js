6c3f45a0e68770f394924a2638589f51ab05f464
Костюков Александр <kostyukov.alex@hotmail.com>
2016-04-25 21:19:31 +0300

    Remove Fix #64358

--- siteMvc/Scripts/Quantumart/Editor/BackendVisualEditor.js
+++ siteMvc/Scripts/Quantumart/Editor/BackendVisualEditor.js
@@ -43,9 +43,14 @@
           classes: true
         }
       },
+      codemirror: {
+        tabSize: 2,
+        indentUnit: 2,
+        smartIndent: false,
+        indentWithTabs: false
+      },
       fillEmptyBlocks: false,
       forcePasteAsPlainText: true,
-      tabSpaces: 0,
       extraAllowedContent: 'replacement',
       height: opts.height || defaultConfig.height,
       resize_dir: 'both',
@@ -80,32 +85,9 @@
     });
 
     config.on = {
-      pluginsLoaded: function(ev) {
-        ev.editor.dataProcessor.dataFilter.addRules({
-          elements: {
-            br: function(element) {
-              if (!element.previous && !element.next && element.name === 'br') {
-                element = undefined;
-                return;
-              }
-
-              if (element.name === 'br' && element.next) {
-                if (element.next.name === 'br') {
-                  return;
-                }
-
-                if(element.next._.isBlockLike) {
-                  new CKEDITOR.htmlParser.text('&nbsp;').insertAfter(element);
-                  return;
-                }
-              }
-            }
-          }
-        });
-      },
       instanceReady: function(ev) {
         ev.editor.filter.addElementCallback(function(el) {
-          // disable attr <--> style transformations
+          // disable attributes to/from style transformations
           if (el.name === 'table' || el.name === 'img') {
             return CKEDITOR.FILTER_SKIP_TREE;
           }
@@ -116,7 +98,6 @@
           CKEDITOR.dtd.$block,
           CKEDITOR.dtd.$listItem,
           CKEDITOR.dtd.$tableContent)).forEach(function(key) {
-          // stop form@t$_#$*&^%... html source code
           this.dataProcessor.writer.setRules(key, {
             indent: false,
             breakBeforeOpen: true,
@@ -125,6 +106,30 @@
             breakAfterClose: false
           });
         }, this);
+
+        this.dataProcessor.writer.setRules('html', {
+          indent: true,
+          breakBeforeOpen: true,
+          breakAfterOpen: true,
+          breakBeforeClose: true,
+          breakAfterClose: true
+        });
+
+        this.dataProcessor.writer.setRules('head', {
+          indent: true,
+          breakBeforeOpen: true,
+          breakAfterOpen: true,
+          breakBeforeClose: true,
+          breakAfterClose: true
+        });
+
+        this.dataProcessor.writer.setRules('body', {
+          indent: true,
+          breakBeforeOpen: true,
+          breakAfterOpen: true,
+          breakBeforeClose: true,
+          breakAfterClose: true
+        });
 
         this.dataProcessor.writer.setRules('br', {
           indent: false,
