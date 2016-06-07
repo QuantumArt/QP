691295b9b050c89785f6bd52ecd91ad599430ebc
Semenets Leonid <semenetsleonid@gmail.com>
2016-06-07 18:30:58 +0300

    Add QP8.FunctionalTests project

---
+++ QP8.FunctionalTests/QP8.FunctionalTests/Tests/BaseTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using System.IO;
+using Nunit3AllureAdapter;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using QP8.FunctionalTests.Configuration;
+using SeleniumExtension;
+using SeleniumExtension.Support.UI;
+
+namespace QP8.FunctionalTests.Tests
+{
+    [AllureTestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class BaseTest : AllureStepDefinition
+    {
+        protected RemoteWebDriver Driver;
+        protected IWait Wait;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            Driver = Setup.GridHub.GetBrowserInstance(DesiredCapabilities.Chrome());
+
+            Driver.Manage().Timeouts().ImplicitlyWait(Config.ImplicitlyTimeout);
+            Driver.Manage().Timeouts().SetPageLoadTimeout(Config.PageLoadTimeout);
+            Driver.Manage().Timeouts().SetScriptTimeout(Config.JavaScriptTimeout);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Manage().Cookies.DeleteAllCookies();
+
+            Wait = ExtensionCore.GetWaiter();
+            Wait.Timeout = Config.ImplicitlyTimeout;
+            Wait.IgnoreExceptionTypes(typeof(NotFoundException), typeof(StaleElementReferenceException));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (!TestContext.CurrentContext.Result.Outcome.Status.Equals(TestStatus.Failed))
+                return;
+
+            using (var memoryStream = new MemoryStream(Driver.GetScreenshot().AsByteArray))
+            {
+                MakeAttachment(Image.FromStream(memoryStream), "Failed screenshot", ImageType.imagePng);
+            }
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Driver.Dispose();
+        }
+    }
+}
---
+++ QP8.FunctionalTests/QP8.PageObjects/Elements/InputBasedElement.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtension.Support.PageObjects.Elements.Implementation;
+
+namespace QP8.PageObjects.Elements
+{
+    /// <summary>
+    /// Объектная модель элемента на основе 'input'<br/>
+    /// <code>
+    /// Примеры вёрстки:
+    /// <br/>
+    /// 1.  &lt;input type=&quot;text&quot;&gt;
+    /// </code>
+    /// </summary>
+    public class InputBasedElement : Element
+    {
+        /// <summary>
+        /// Получение текста элемента<br/>
+        /// Выполняется путём поиска дочернего элемента 'input' и получения его текста
+        /// </summary>
+        public new string Text
+        {
+            get
+            {
+                return ProxyWebElement.FindElement(By.CssSelector("input")).GetAttribute("value");
+            }
+        }
+
+        public InputBasedElement(IWebElement webElement, IWebDriver webDriver) 
+            : base(webElement, webDriver)
+        {
+        }
+    }
+}
