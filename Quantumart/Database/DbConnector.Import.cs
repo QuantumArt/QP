c235ea90262ac36ed8c170e5b95eeb08dc9578ef
Pavel Celut <CelutP@quantumart.ru>
2016-06-22 13:26:59 +0300

    Fix empty M2M #68250

--- Quantumart/Database/DbConnector.Import.cs
+++ Quantumart/Database/DbConnector.Import.cs
@@ -117,8 +117,10 @@ namespace Quantumart.QPublishing.Database
                     linkElem.Add(new XAttribute("id", value[SystemColumnNames.Id]));
                     if (attr.LinkId != null) linkElem.Add(new XAttribute("linkId", attr.LinkId.Value));
                     if (value.TryGetValue(attr.Name, out temp))
+                    {
                         linkElem.Add(new XAttribute("value", temp));
-                    linkDoc.Root?.Add(linkElem);
+                        linkDoc.Root?.Add(linkElem);
+                    }
                 }
             }
             return linkDoc;
