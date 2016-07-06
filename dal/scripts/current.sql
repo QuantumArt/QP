2c7ee4facf9097ecd3826f9a2585f29255fddcbc
Pavel Celut <CelutP@quantumart.ru>
2016-07-06 13:19:35 +0300

    Prepare for release #64445

--- dal/scripts/current.sql
+++ dal/scripts/current.sql
@@ -538,3 +538,35 @@ GO
 
 GO
 
+if not exists (select * From BACKEND_ACTION where code = 'select_child_articles')
+begin
+
+	INSERT INTO [dbo].[BACKEND_ACTION] ([TYPE_ID], [ENTITY_TYPE_ID], [NAME], [SHORT_NAME], [CODE], [IS_INTERFACE])
+	VALUES (dbo.qp_action_type_id('select'), dbo.qp_entity_type_id('article'), N'Select Child Articles', 'Select Child Articles', N'select_child_articles', 0)
+
+	INSERT INTO [dbo].[CONTEXT_MENU_ITEM] ([CONTEXT_MENU_ID], [ACTION_ID], [Name], [ORDER], [BOTTOM_SEPARATOR])
+	VALUES (dbo.qp_context_menu_id('article'), dbo.qp_action_id('select_child_articles'), N'Select Child Articles', 80, 0)
+
+end
+
+if not exists (select * From BACKEND_ACTION where code = 'unselect_child_articles')
+begin
+
+	INSERT INTO [dbo].[BACKEND_ACTION] ([TYPE_ID], [ENTITY_TYPE_ID], [NAME], [SHORT_NAME], [CODE], [IS_INTERFACE])
+	VALUES (dbo.qp_action_type_id('select'), dbo.qp_entity_type_id('article'), N'Unselect Child Articles', 'Unselect Child Articles', N'unselect_child_articles', 0)
+
+	INSERT INTO [dbo].[CONTEXT_MENU_ITEM] ([CONTEXT_MENU_ID], [ACTION_ID], [Name], [ORDER], [BOTTOM_SEPARATOR])
+	VALUES (dbo.qp_context_menu_id('article'), dbo.qp_action_id('unselect_child_articles'), N'Unselect Child Articles', 90, 0)
+
+end
+
+update CONTEXT_MENU_ITEM set icon = 'deselect_all.gif' where ACTION_ID = dbo.qp_action_id('unselect_child_articles')
+update CONTEXT_MENU_ITEM set icon = 'select_all.gif' where ACTION_ID = dbo.qp_action_id('select_child_articles')
+
+exec qp_update_translations 'Select Child Articles', 'Выбрать дочерние статьи'
+exec qp_update_translations 'Unselect Child Articles', 'Отменить выбор дочерних статей'
+
+
+
+GO
+
