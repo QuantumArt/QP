b6f3183d931ec5d7c0c2fd16f861c98dcf02d504
Костюков Александр <kostyukov.alex@hotmail.com>
2016-09-20 13:17:04 +0300

    move jsfiles and revisions based on project.json version

---
+++ siteMvc/Properties/AssemblyVersion.base.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyConfiguration("development")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyDescription("Build: 0, Sha: 0")]
