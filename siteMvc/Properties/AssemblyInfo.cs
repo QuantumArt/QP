2f6789223f7b7f559234b9161f00603fd390721f
Костюков Александр <kostyukov.alex@hotmail.com>
2016-10-20 19:07:59 +0300

    fix version

--- siteMvc/Properties/AssemblyInfo.cs
+++ siteMvc/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("QP8 Backend Web MVC Project")]
 [assembly: AssemblyDescription("Local builded at 2016-10-20")]
 
-[assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.1.0+.Sha.0")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyInformationalVersion("0.1.2.0+.Sha.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("bf0fce66-3590-472b-abf9-2dcee321929c")]
