c4935dc36d346dacc59136c5e660067b899a6053
Pavel Celut <CelutP@quantumart.ru>
2017-01-30 21:57:29 +0300

    styling fixes

--- plugins/beeline_catalog/dpc.js
+++ plugins/beeline_catalog/dpc.js
@@ -9,17 +9,16 @@
 
 
 Quantumart.QP8.BackendDocumentContext.setGlobal("typesByProduct", {
-  "289" : "2173", "305" : "2173", 
-  "290" : "2174", "312" : "2174", 
-  "362" : $ctx.getGlobal("typeIds")["tariff_b2b"], "360" : $ctx.getGlobal("typeIds")["tariff_b2b"], 
-  "361" : $ctx.getGlobal("typeIds")["service_b2b"], "363" : $ctx.getGlobal("typeIds")["service_b2b"],
-  "364" : $ctx.getGlobal("typeIds")["equipment_b2b"], "365" : $ctx.getGlobal("typeIds")["equipment_b2b"],
-  "311" : "2414", 
+  "289" : "2173", "305" : "2173",
+  "290" : "2174", "312" : "2174",
+  "362" : $ctx.getGlobal("typeIds").tariff_b2b, "360" : $ctx.getGlobal("typeIds").tariff_b2b,
+  "361" : $ctx.getGlobal("typeIds").service_b2b, "363" : $ctx.getGlobal("typeIds").service_b2b,
+  "364" : $ctx.getGlobal("typeIds").equipment_b2b, "365" : $ctx.getGlobal("typeIds").equipment_b2b,
+  "311" : "2414",
   "308" : "2413",
   "309" : "2132",
-  "366" : $ctx.getGlobal("typeIds")["upsale_b2b"],
-  "436" : $ctx.getGlobal("typeIds")["banner_veon"]  
-
+  "366" : $ctx.getGlobal("typeIds").upsale_b2b,
+  "436" : $ctx.getGlobal("typeIds").banner_veon
 });
 
 Quantumart.QP8.BackendDocumentContext.prototype.getTypeByProduct = function(product) {
@@ -27,38 +26,33 @@ Quantumart.QP8.BackendDocumentContext.prototype.getTypeByProduct = function(prod
 };
 
 Quantumart.QP8.BackendDocumentContext.prototype.initHandler = function(editor, $elem) {
-  if (this.changeFilters)
-  {
-    this.changeFilters(editor, this.getCurrentType(editor), $elem);      
+  if (this.changeFilters) {
+    this.changeFilters(editor, this.getCurrentType(editor), $elem);
   }
-  
-  if (this.changeFieldState)
-  {
+
+  if (this.changeFieldState) {
     this.changeFieldState(editor, null, $elem);
   }
 };
 
 Quantumart.QP8.BackendDocumentContext.prototype.fieldValueChangedHandler = function(editor, data) {
-  if ((data.fieldName == this.typeResolverInputName || data.contentFieldName == this.typeResolverInputName) && this.changeFilters)
-  {
+  if ((data.fieldName === this.typeResolverInputName || data.contentFieldName === this.typeResolverInputName) && this.changeFilters) {
     this.changeFilters(editor, this.getCurrentType(editor));
   }
-  
-  if (this.changeFieldState)
-  {
+
+  if (this.changeFieldState) {
     this.changeFieldState(editor, data, null);
   }
-  
-}; 
- 
-Quantumart.QP8.BackendDocumentContext.prototype.setFilter = function(inputName, value, fieldName, editor)
-{
-  var filter = (fieldName) ? 
+};
+
+Quantumart.QP8.BackendDocumentContext.prototype.setFilter = function(inputName, value, fieldName, editor) {
+  var filter = (fieldName) ?
     "c.[" + fieldName + "] = " + value :
     "c.content_item_id in (select linked_item_id from item_link where item_id = " + value + ")";
   var list = this.getRow(editor, inputName).find(".dataList").data("entity_data_list_component");
-  if (list)
+  if (list) {
     list.applyFilter(filter);
+  }
 };
 
 Quantumart.QP8.BackendDocumentContext.prototype.getParentContentId = function(editor) {
@@ -66,12 +60,10 @@ Quantumart.QP8.BackendDocumentContext.prototype.getParentContentId = function(ed
 };
 
 Quantumart.QP8.BackendDocumentContext.prototype.getCurrentType = function(editor) {
-  if (!this.typeResolverInputName)
-  {
+  if (!this.typeResolverInputName) {
     return 0;
   }
-  else
-  {
+  else {
     var currentTypeResolverValue = this.getElem(editor, this.typeResolverInputName).val();
     return this.getType(editor, currentTypeResolverValue);
   }
@@ -79,74 +71,67 @@ Quantumart.QP8.BackendDocumentContext.prototype.getCurrentType = function(editor
 
 Quantumart.QP8.BackendDocumentContext.prototype.getType = function(editor, val) {
   var prod = 0;
-  if (this.parentTypeFieldName)
-  {
+  if (this.parentTypeFieldName) {
     prod = (!+val) ? 0 :$o.getArticleFieldValue(this.getParentContentId(editor), this.parentTypeFieldName, val);
   }
-  else
-  {
+  else {
     prod = this.getValue(editor, this.typeResolverInputName);
   }
-  return this.getTypeByProduct(prod)
+  return this.getTypeByProduct(prod);
 };
 
-Quantumart.QP8.BackendDocumentContext.prototype.changeFilters = function(editor, value, $elem)
-{
-    if (value && this.typeFilters)
-    {
-      for (var key in this.typeFilters)
-      {
+Quantumart.QP8.BackendDocumentContext.prototype.changeFilters = function(editor, value, $elem) {
+    if (value && this.typeFilters) {
+      for (var key in this.typeFilters) {
         this.setFilter(key, value, this.typeFilters[key], editor);
       }
     }
 };
 
-Quantumart.QP8.BackendDocumentContext.prototype.toggleVeButtons = function(editor, inputName, state)
-{
+Quantumart.QP8.BackendDocumentContext.prototype.toggleVeButtons = function(editor, inputName, state) {
    var $elem = this.getRow(editor, inputName);
    var $button = $elem.find(".visualEditorToolbar");
-   if (state)
+   if (state) {
        $button.show();
-   else
+   }
+   else {
        $button.hide();
+   }
 };
 
-Quantumart.QP8.BackendDocumentContext.prototype.toggleField = function(editor, inputName, state, preserveHidden)
-{
+Quantumart.QP8.BackendDocumentContext.prototype.toggleField = function(editor, inputName, state, preserveHidden) {
   var $elem = this.getRow(editor, inputName);
   var list = $elem.find(".dataList").data("entity_data_list_component");
-  if (state) 
-  {
-    $elem.show(); 
-    if (list)
-    list._fixListOverflow();
+  if (state) {
+    $elem.show();
+    if (list) {
+      list._fixListOverflow();
+    }
   }
-  else 
+  else
   {
     $elem.hide();
     if (!preserveHidden)
     {
-      if (list)
+      if (list) {
         list.selectEntities();
-      else
+      } else {
         $elem.val("");
-    }   
+      }
+    }
   }
 };
 
-Quantumart.QP8.BackendDocumentContext.prototype.getValue = function(editor, inputName)
-{
+Quantumart.QP8.BackendDocumentContext.prototype.getValue = function(editor, inputName) {
   return this.getElem(editor, inputName).val();
 };
 
-Quantumart.QP8.BackendDocumentContext.prototype.getElem = function(editor, inputName)
-{
-  var $form = (editor instanceof jQuery) ? editor : jQuery(editor._formElement);  
+Quantumart.QP8.BackendDocumentContext.prototype.getElem = function(editor, inputName) {
+  var $form = (editor instanceof jQuery) ? editor : jQuery(editor._formElement);
   return $form.find("[name='" + inputName + "'],[data-content_field_name='" + inputName + "']");
 };
 
-Quantumart.QP8.BackendDocumentContext.prototype.getRow = function(editor, inputName)
-{
+Quantumart.QP8.BackendDocumentContext.prototype.getRow = function(editor, inputName) {
   var $form = (editor instanceof jQuery) ? editor : jQuery(editor._formElement);
   return $form.find("dl[data-field_form_name='" + inputName + "'],dl[data-field_name='" + inputName + "']");
 };
--- plugins/beeline_catalog/translit.js
+++ plugins/beeline_catalog/translit.js
@@ -1,98 +1,94 @@
 ﻿Quantumart.QP8.BackendDocumentContext.prototype.notifyCustomButtonExistence = false;
 
-Quantumart.QP8.BackendDocumentContext.setGlobal('Transliterator',
-      {
-      symbols: {
-      'а': 'a',
-      'б': 'b',
-      'в': 'v',
-      'г': 'g',
-      'д': 'd',
-      'е': 'e',
-      'ё': 'e',
-      'ж': 'zh',
-      'з': 'z',
-      'и': 'i',
-      'й': 'y',
-      'к': 'k',
-      'л': 'l',
-      'м': 'm',
-      'н': 'n',
-      'о': 'o',
-      'п': 'p',
-      'р': 'r',
-      'с': 's',
-      'т': 't',
-      'у': 'u',
-      'ф': 'f',
-      'х': 'h',
-      'ц': 'c',
-      'ч': 'ch',
-      'ш': 'sh',
-      'щ': 'shh',
-      'ы': 'i',
-      'э': 'e',
-      'ю': 'yu',
-      'я': 'ya',
-      ' ': '-'
-      },
+Quantumart.QP8.BackendDocumentContext.setGlobal('Transliterator', {
+  symbols: {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'й': 'y',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'h',
+    'ц': 'c',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'shh',
+    'ы': 'i',
+    'э': 'e',
+    'ю': 'yu',
+    'я': 'ya',
+    ' ': '-'
+  },
 
-      dashCode: "-".charCodeAt(0),
-      enLettersStartCode: "a".charCodeAt(0),
-      enLettersEndCode: "z".charCodeAt(0),
-      digitsStartCode: "0".charCodeAt(0),
-      digitsEndCode: "9".charCodeAt(0),
+  dashCode: "-".charCodeAt(0),
+  enLettersStartCode: "a".charCodeAt(0),
+  enLettersEndCode: "z".charCodeAt(0),
+  digitsStartCode: "0".charCodeAt(0),
+  digitsEndCode: "9".charCodeAt(0),
 
-      transliterate: function (input) {
+  transliterate: function (input) {
 
-      input = input.toLowerCase();
+  input = input.toLowerCase();
 
-      var result = "";
-
-      for (var i = 0; i < input.length; i++) {
-
+    var result = "";
+    for (var i = 0; i < input.length; i++) {
       var currentSymbol = input[i];
-
       var symbolToReplace = this.symbols[currentSymbol];
 
       if (symbolToReplace != undefined) {
-      result += symbolToReplace;
+        result += symbolToReplace;
       }
       else {
-      var currentSymbolCode = currentSymbol.charCodeAt(0);
-
-      if (currentSymbolCode == this.dashCode
-      || currentSymbolCode >= this.enLettersStartCode && currentSymbolCode <= this.enLettersEndCode
-      || currentSymbolCode >= this.digitsStartCode && currentSymbolCode <= this.digitsEndCode)
-      result += currentSymbol;
+        var currentSymbolCode = currentSymbol.charCodeAt(0);
+        if (currentSymbolCode == this.dashCode
+        || currentSymbolCode >= this.enLettersStartCode && currentSymbolCode <= this.enLettersEndCode
+        || currentSymbolCode >= this.digitsStartCode && currentSymbolCode <= this.digitsEndCode) {
+          result += currentSymbol;
+        }
       }
-      }
+    }
 
-      result = result.replace(/-+/g, "-");
+    result = result.replace(/-+/g, "-");
 
-      if (result.length > 0 && result[0] == "-")
+    if (result.length > 0 && result[0] == "-") {
       result = result.substr(1);
+    }
 
-      if (result.length > 0 && result[result.length - 1] == "-")
+    if (result.length > 0 && result[result.length - 1] == "-") {
       result = result.substr(0, result.length - 1);
+    }
+    return result;
+  }
+});
 
-      return result;
-      }
-      });
-
-Quantumart.QP8.BackendDocumentContext.prototype.addTransliterateButton = function (srcInputName, dstInputName)
-{
-    var self = this;
-    this.addCustomLinkButton({
-      name: dstInputName,
-      title: "Transliterate",
-      suffix: "translit",
-      "class": "customLinkButton",
-      url: "/Backend/Content/QP8/icons/16x16/insert_call.gif",
-      onClick: function (evt) {
-		var resultInput = evt.data.$input;
-		var textToProcess = self.getValue(evt.data.$form, srcInputName);
-		resultInput.val($ctx.getGlobal('Transliterator').transliterate(textToProcess)).trigger("change");
-      }
-    });
+Quantumart.QP8.BackendDocumentContext.prototype.addTransliterateButton = function (srcInputName, dstInputName) {
+  var self = this;
+  this.addCustomLinkButton({
+    name: dstInputName,
+    title: "Transliterate",
+    suffix: "translit",
+    "class": "customLinkButton",
+    url: "/Backend/Content/QP8/icons/16x16/insert_call.gif",
+    onClick: function (evt) {
+      var resultInput = evt.data.$input;
+      var textToProcess = self.getValue(evt.data.$form, srcInputName);
+      resultInput.val($ctx.getGlobal('Transliterator').transliterate(textToProcess)).trigger("change");
+    }
+  });
 };
