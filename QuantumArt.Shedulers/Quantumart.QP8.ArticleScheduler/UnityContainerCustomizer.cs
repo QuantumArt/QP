a6d5fb396cf97e74d55320de1803cfd594fb1c97
Костюков Александр <kostyukov.alex@hotmail.com>
2017-03-29 19:00:30 +0300

    Revert unity 4 update

--- QuantumArt.Shedulers/Quantumart.QP8.ArticleScheduler/UnityContainerCustomizer.cs
+++ QuantumArt.Shedulers/Quantumart.QP8.ArticleScheduler/UnityContainerCustomizer.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Practices.Unity;
-using QP8.Infrastructure.Logging;
-using QP8.Infrastructure.Logging.Factories;
-using QP8.Infrastructure.Logging.Interfaces;
+using QP8.Infrastructure.Logging.UnityExtensions;
 using Quantumart.QP8.BLL.Services.API.ArticleScheduler;
 using Quantumart.QP8.BLL.Services.ArticleScheduler;
 
@@ -12,18 +10,13 @@ namespace Quantumart.QP8.ArticleScheduler
         static UnityContainerCustomizer()
         {
             UnityContainer = new UnityContainer()
-                .RegisterType<IArticleSchedulerService, ArticleSchedulerService>()
-                .RegisterType<IArticleOnetimeSchedulerService, ArticleSchedulerService>()
-                .RegisterType<IArticlePublishingSchedulerService, ArticleSchedulerService>()
-                .RegisterType<IArticleRecurringSchedulerService, ArticleSchedulerService>()
-                .RegisterType<IExceptionHandler, ExceptionHandler>()
-                .RegisterType<IOperationsLogWriter, OperationsLogWriter>();
-            // TODO: Uncomment when update Unity to 4.0 .AddNewExtension<NLogContainerExtension>();
-
-            UnityContainer.RegisterType<ILogFactory, NLogFactory>(new ContainerControlledLifetimeManager());
-            UnityContainer.RegisterType<ILog>(new InjectionFactory(c => c.Resolve<ILogFactory>().GetLogger()));
-            LogProvider.LogFactory = UnityContainer.Resolve<ILogFactory>();
-            Logger.Log = UnityContainer.Resolve<ILog>();
+            .RegisterType<IArticleSchedulerService, ArticleSchedulerService>()
+            .RegisterType<IArticleOnetimeSchedulerService, ArticleSchedulerService>()
+            .RegisterType<IArticlePublishingSchedulerService, ArticleSchedulerService>()
+            .RegisterType<IArticleRecurringSchedulerService, ArticleSchedulerService>()
+            .RegisterType<IExceptionHandler, ExceptionHandler>()
+            .RegisterType<IOperationsLogWriter, OperationsLogWriter>()
+            .AddNewExtension<NLogContainerExtension>();
         }
 
         public static IUnityContainer UnityContainer { get; }
