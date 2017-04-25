f72458abde31f99b85e0af6206be761201d6ebb5
Sergey Koryakin <KoryakinS@quantumart.ru>
2017-04-25 11:36:15 +0300

    update current.sql #95431

--- dal/scripts/current.sql
+++ dal/scripts/current.sql
@@ -1694,4 +1694,8 @@ END
 GO
 
 GO
+IF NOT EXISTS (  SELECT * FROM   sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[DB]') AND name = 'USE_DPC')
+	ALTER TABLE [dbo].[DB] ADD [USE_DPC] [bit] NOT NULL DEFAULT ((0))
+
+GO
 
