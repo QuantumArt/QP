1e76c51f6feb05b3692184f503119123fad70c9c
Костюков Александр <kostyukov.alex@hotmail.com>
2017-04-26 18:49:05 +0300

    update project and remove unused code

--- QP8.TestCustomActionsHost/Controllers/ActionController.cs
+++ QP8.TestCustomActionsHost/Controllers/ActionController.cs
@@ -1,5 +1,4 @@
 using System.Web.Mvc;
-using QP8.TestCustomActionsHost;
 using QP8.TestCustomActionsHost.Models;
 
 namespace QP8.TestCustomActionsHost.Controllers
@@ -7,14 +6,11 @@ namespace QP8.TestCustomActionsHost.Controllers
     [ValidateInput(false)]
     public class ActionController : Controller
     {
-        //
-        // GET: /ActionOne/
-
-        public ActionResult Index(string hostUID)
+        public ActionResult Index(string hostUid)
         {
             return View(new BackendActionViewModel
             {
-                HostUID = hostUID
+                HostUid = hostUid
             });
         }
 
@@ -28,11 +24,11 @@ namespace QP8.TestCustomActionsHost.Controllers
             return Content("OK");
         }
 
-        public ActionResult ContextIndex(string hostUID)
+        public ActionResult ContextIndex(string hostUid)
         {
             return View(new BackendActionViewModel
             {
-                HostUID = hostUID,
+                HostUid = hostUid,
                 EntityId = 106401,
                 ParentEntityId = 339
             });
@@ -41,8 +37,7 @@ namespace QP8.TestCustomActionsHost.Controllers
         [HttpGet]
         public ActionResult NonInterface()
         {
-            return new JsonpResult {Data = null, JsonRequestBehavior=JsonRequestBehavior.AllowGet };
+            return new JsonpResult { Data = null, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
-
     }
 }
--- QP8.TestCustomActionsHost/Models/BackendActionViewModel.cs
+++ QP8.TestCustomActionsHost/Models/BackendActionViewModel.cs
@@ -1,28 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
 namespace QP8.TestCustomActionsHost.Models
 {
-	public class BackendActionViewModel
-	{
+    public class BackendActionViewModel
+    {
 
-		public BackendActionViewModel()
-		{
-			Title = "Test integration";
-			ActionCode = "edit_article";
-			EntityTypeCode = "article";
-			EntityId = 106401;
-			ParentEntityId = 339;
+        public BackendActionViewModel()
+        {
+            Title = "Test integration";
+            ActionCode = "edit_article";
+            EntityTypeCode = "article";
+            EntityId = 106401;
+            ParentEntityId = 339;
+        }
 
-		}
+        public string HostUid { get; set; }
 
-		public string HostUID { get; set; }
-		public string Title { get; set; }
-		public string ActionCode { get; set; }
-		public string EntityTypeCode { get; set; }
-		public int EntityId { get; set; }
-		public int ParentEntityId { get; set; }
-	}
+        public string Title { get; set; }
+
+        public string ActionCode { get; set; }
+
+        public string EntityTypeCode { get; set; }
+
+        public int EntityId { get; set; }
+
+        public int ParentEntityId { get; set; }
+    }
 }
