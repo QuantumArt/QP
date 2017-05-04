adf47f80aa8ee810927eab94dd0e481fb5860e8c
Pavel Celut <CelutP@quantumart.ru>
2017-05-04 12:08:11 +0300

    Add columns and new queue

---
+++ dal/scripts/DPC/1.DbAddFields.sql
@@ -0,0 +1,7 @@
+IF NOT EXISTS (  SELECT * FROM   sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[DB]') AND name = 'USE_DPC')
+	ALTER TABLE [dbo].[DB] ADD [USE_DPC] [bit] NOT NULL DEFAULT ((0))
+GO
+
+IF NOT EXISTS (  SELECT * FROM   sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[DB]') AND name = 'USE_CDC')
+	ALTER TABLE [dbo].[DB] ADD [USE_CDC] [bit] NOT NULL DEFAULT ((0))
+GO
