ecfcb9b3b42fa117dd8be8238b00e23edfebbc4c
Костюков Александр <kostyukov.alex@hotmail.com>
2017-06-29 19:01:27 +0300

    remove unused files

--- plugins/beeline_cis/shop.js
+++
@@ -1,4 +0,0 @@
-Quantumart.QP8.BackendDocumentContext.prototype.initHandler = function(editor, $elem) {
-	editor._hideFields = jQuery.isArray(editor._hideFields) ? editor._hideFields.concat(this.fieldsToHide) : this.fieldsToHide;
-};
-
