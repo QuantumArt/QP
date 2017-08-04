e407d8c4f217e0e14e9fc6a37179afb7f7918b6f
Костюков Александр <kostyukov.alex@hotmail.com>
2017-08-04 13:23:57 +0300

    fix sql scripts

--- dal/scripts/CDC/MaxTextReplSize.sql
+++
@@ -1,4 +0,0 @@
-EXEC sp_configure 'show advanced options', 1;
-EXEC sp_configure 'max text repl size', -1;
-RECONFIGURE;
-GO
--- dal/scripts/DPC/7.CONTENT_DATA.sql
+++
@@ -1,5 +0,0 @@
-IF NOT EXISTS (  SELECT * FROM   sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[CONTENT_DATA]') AND name = 'SPLITTED')
-    ALTER TABLE [dbo].[CONTENT_DATA] ADD [SPLITTED] [bit] NOT NULL DEFAULT ((0))
-GO
-
-
