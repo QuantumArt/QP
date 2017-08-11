03f2d209a614efe5f6b49aeb9e4f8506145a2c7a
Костюков Александр <kostyukov.alex@hotmail.com>
2017-08-11 16:31:27 +0300

    fix yoda

--- siteMvc/Scripts/Quantumart/ckeditor/codemirror/js/codemirror.js
+++ siteMvc/Scripts/Quantumart/ckeditor/codemirror/js/codemirror.js
@@ -980,10 +980,10 @@ var bidiOrdering = (function () {
   var arabicTypes = "nnnnnnNNr%%r,rNNmmmmmmmmmmmrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrmmmmmmmmmmmmmmmmmmmmmnnnnnnnnnn%nnrrrmrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrmmmmmmmnNmmmmmmrrmmNmmmmrr1111111111";
   function charType(code) {
     if (code <= 0xf7) { return lowTypes.charAt(code); }
-    else if (0x590 <= code && code <= 0x5f4) { return "R"; }
-    else if (0x600 <= code && code <= 0x6f9) { return arabicTypes.charAt(code - 0x600); }
-    else if (0x6ee <= code && code <= 0x8ac) { return "r"; }
-    else if (0x2000 <= code && code <= 0x200b) { return "w"; }
+    else if (code >= 0x590 && code <= 0x5f4) { return "R"; }
+    else if (code >= 0x600 && code <= 0x6f9) { return arabicTypes.charAt(code - 0x600); }
+    else if (code >= 0x6ee && code <= 0x8ac) { return "r"; }
+    else if (code >= 0x2000 && code <= 0x200b) { return "w"; }
     else if (code == 0x200c) { return "b"; }
     else { return "L"; }
   }
