2103415e8116c824b79ce5075a36811deff36869
Sergey Koryakin <KoryakinS@quantumart.ru>
2017-12-27 17:58:17 +0300

    add product versions data #113039

---
+++ dal/scripts/AddProductVersions/0. drop_existing.sql
@@ -0,0 +1,3 @@
+
+exec qp_drop_existing  'GetProducts', 'IsTableFunction'
+GO
---
+++ dal/scripts/AddProductVersions/1. ProductVersions.sql
@@ -0,0 +1,40 @@
+if not exists(select * from sys.tables where name = 'ProductVersions')
+BEGIN
+	CREATE TABLE [dbo].[ProductVersions](
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[Deleted] [bit] NOT NULL,
+		[Modification] [datetime] NOT NULL,
+		[DpcId] [int] NOT NULL,
+		[Slug] [nvarchar](50) NULL,
+		[Version] [int] NOT NULL,
+		[IsLive] [bit] NOT NULL,
+		[Language] [nvarchar](10) NULL,
+		[Format] [nvarchar](10) NOT NULL,
+		[Data] [nvarchar](max) NOT NULL,
+		[Alias] [nvarchar](250) NULL,
+		[Created] [datetime] NOT NULL,
+		[Updated] [datetime] NOT NULL,
+		[Hash] [nvarchar](2000) NOT NULL,
+		[MarketingProductId] [int] NULL,
+		[Title] [nvarchar](500) NULL,
+		[UserUpdated] [nvarchar](50) NULL,
+		[UserUpdatedId] [int] NULL,
+		[ProductType] [nvarchar](250) NULL
+		CONSTRAINT [PK_ProductVersions] PRIMARY KEY CLUSTERED ([Id] ASC )	
+	)
+END
+GO
+
+if not exists (select * from sys.indexes where name = 'IX_ProductVersions')
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_ProductVersions] ON [dbo].[Products]
+    (
+        [DpcId] ASC,
+        [Slug] ASC,
+        [Version] ASC,
+        [IsLive] ASC,
+        [Language] ASC,
+        [Format] ASC
+    )
+END
+GO
