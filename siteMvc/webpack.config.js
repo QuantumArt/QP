b2f13ad218e3fbf85113dce700a9eed5b5c29fcd
PanyushkinD <PanyushkinD@quantumart.ru>
2017-12-28 15:44:01 +0300

    Improve webpack config

--- siteMvc/webpack.config.js
+++ siteMvc/webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 const webpack = require('webpack');
 const { resolve } = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase();
 
@@ -8,14 +9,17 @@ const PRODUCTION = ['relesase', 'production'].includes(NODE_ENV);
 
 module.exports = {
   devtool: PRODUCTION ? 'source-map' : 'eval-source-map',
+
   entry: {
     'app': resolve(__dirname, 'Scripts/Quantumart/App.js'),
     'app-logon': resolve(__dirname, 'Scripts/Quantumart/AppLogon.js'),
   },
+
   output: {
     path: resolve(__dirname, 'Scripts/build'),
     filename: '[name].js',
   },
+
   module: {
     rules: [{
       test: /\.(js|jsx)?$/,
@@ -27,9 +31,17 @@ module.exports = {
       }
     }]
   },
+
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: resolve(__dirname, 'tsconfig.json')
+      })
+    ]
   },
+
   plugins: compact([
     PRODUCTION && new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
