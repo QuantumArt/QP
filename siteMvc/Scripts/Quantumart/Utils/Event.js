b148e15f417e32a87c46f68626a8b98927bfbf53
PanyushkinD <PanyushkinD@quantumart.ru>
2018-01-17 16:52:12 +0300

    Improve Event

--- siteMvc/Scripts/Quantumart/Utils/Event.js
+++ siteMvc/Scripts/Quantumart/Utils/Event.js
@@ -1,24 +1,30 @@
 /**
  * Factory that creates event function.
  * Which delegates it's execution to list of handlers.
+ * @param {object} sender Sender object
  * @returns Event function
  */
-export const event = function () {
+export const event = function (sender) {
   const listeners = [];
 
   const e = function (...args) {
-    listeners.slice().forEach(handler => {
-      handler(...args);
-    });
+    const promises = listeners
+      .slice()
+      .map(handler => handler(sender, ...args))
+      .filter(promise => promise instanceof Promise);
+
+    return Promise.all(promises);
   };
 
   e.attach = handler => {
     listeners.push(handler);
+    return e;
   };
 
   e.detach = handler => {
     // eslint-disable-next-line no-bitwise
     listeners.splice(listeners.indexOf(handler) >>> 0, 1);
+    return e;
   };
 
   return e;
