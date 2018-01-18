1ec04cb8cd54e68f7cd2d2d3bc1ec8eb97e54908
PanyushkinD <PanyushkinD@quantumart.ru>
2018-01-18 11:39:38 +0300

    Refactor Event

--- siteMvc/Scripts/Quantumart/Utils/Event.d.ts
+++ siteMvc/Scripts/Quantumart/Utils/Event.d.ts
@@ -5,11 +5,54 @@
 export declare function event<TSender>(
   sender: TSender,
 ): Event<
+  (...args: any[]) => void,
+  (sender: TSender, ...args: any[]) => void
+>;
+
+export declare function event<TSender>(
+  sender: TSender,
+  dataType: new () => String,
+): Event<
+  (data: string) => void,
+  (sender: TSender, data: string) => void
+>;
+
+export declare function event<TSender>(
+  sender: TSender,
+  dataType: new () => Number,
+): Event<
+  (data: number) => void,
+  (sender: TSender, data: number) => void
+>;
+
+export declare function event<TSender>(
+  sender: TSender,
+  dataType: new () => Boolean,
+): Event<
+  (data: boolean) => void,
+  (sender: TSender, data: boolean) => void
+>;
+
+export declare function event<TSender, TData>(
+  sender: TSender,
+  dataType: new () => TData,
+): Event<
+  (data: TData) => void,
+  (sender: TSender, data: TData) => void
+>;
+
+/**
+ * Factory that creates async event function.
+ * Which delegates it's execution to list of handlers.
+ */
+export declare function asyncEvent<TSender>(
+  sender: TSender,
+): Event<
   (...args: any[]) => Promise<void>,
   (sender: TSender, ...args: any[]) => Promise<void> | void
 >;
 
-export declare function event<TSender>(
+export declare function asyncEvent<TSender>(
   sender: TSender,
   dataType: new () => String,
 ): Event<
@@ -17,7 +60,7 @@ export declare function event<TSender>(
   (sender: TSender, data: string) => Promise<void> | void
 >;
 
-export declare function event<TSender>(
+export declare function asyncEvent<TSender>(
   sender: TSender,
   dataType: new () => Number,
 ): Event<
@@ -25,7 +68,7 @@ export declare function event<TSender>(
   (sender: TSender, data: number) => Promise<void> | void
 >;
 
-export declare function event<TSender>(
+export declare function asyncEvent<TSender>(
   sender: TSender,
   dataType: new () => Boolean,
 ): Event<
@@ -33,7 +76,7 @@ export declare function event<TSender>(
   (sender: TSender, data: boolean) => Promise<void> | void
 >;
 
-export declare function event<TSender, TData>(
+export declare function asyncEvent<TSender, TData>(
   sender: TSender,
   dataType: new () => TData,
 ): Event<
--- siteMvc/Scripts/Quantumart/Utils/Event.js
+++ siteMvc/Scripts/Quantumart/Utils/Event.js
@@ -8,10 +8,42 @@ export const event = function (sender) {
   const listeners = [];
 
   const e = function (...args) {
+    listeners.slice().forEach(handler => {
+      handler(sender, ...args);
+    });
+  };
+
+  e.attach = handler => {
+    listeners.push(handler);
+    return e;
+  };
+
+  e.detach = handler => {
+    // eslint-disable-next-line no-bitwise
+    listeners.splice(listeners.indexOf(handler) >>> 0, 1);
+    return e;
+  };
+
+  return e;
+};
+
+/**
+ * Factory that creates async event function.
+ * Which delegates it's execution to list of handlers.
+ * @param {object} sender Sender object
+ * @returns Event function
+ */
+export const asyncEvent = function (sender) {
+  const listeners = [];
+
+  const e = function (...args) {
     const promises = listeners
       .slice()
       .map(handler => handler(sender, ...args))
-      .filter(promise => promise instanceof Promise);
+      .filter(promise => promise !== null
+        && typeof promise === 'object'
+        && typeof promise.then === 'function'
+      );
 
     return Promise.all(promises);
   };
