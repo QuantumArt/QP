f279d8efc965968022be87a1f65483328c9d9826
Sergey Koryakin <KoryakinS@quantumart.ru>
2018-02-02 18:13:59 +0300

    delete ProductRegionVersions #113039

--- dal/scripts/AddProductVersions/4.SyncProductVersions.sql
+++ dal/scripts/AddProductVersions/4.SyncProductVersions.sql
@@ -51,40 +51,5 @@ BEGIN
 	END
 
 	print('End update ProductVersions')
-	print('Start update ProductRegionVersions...')
-
-	WHILE exists(
-		select null
-		from
-			Products p
-			join ProductVersions v on
-				p.[Updated] = v.[Modification] and
-				p.[DpcId] = v.[DpcId] and
-				p.[IsLive] = v.[IsLive] and
-				p.[Language] = v.[Language]
-				and p.[Format] = v.[Format]
-			join ProductRegions r on
-				p.Id = r.ProductId
-		where
-			not exists (select null from ProductRegionVersions rv with (nolock) where rv.[ProductVersionId] = v.[Id])
-	)
-	BEGIN
-		insert ProductRegionVersions([ProductVersionId], [RegionId])
-		select top 10000 v.[Id], r.[RegionId]
-		from
-			Products p
-			join ProductVersions v on
-				p.[Updated] = v.[Modification] and
-				p.[DpcId] = v.[DpcId] and
-				p.[IsLive] = v.[IsLive] and
-				p.[Language] = v.[Language]
-				and p.[Format] = v.[Format]
-			join ProductRegions r on
-				p.Id = r.ProductId
-		where
-			not exists (select null from ProductRegionVersions rv with (nolock) where rv.[ProductVersionId] = v.[Id])
-	END
-
-	print('End update ProductRegionVersions')
 END
 GO
