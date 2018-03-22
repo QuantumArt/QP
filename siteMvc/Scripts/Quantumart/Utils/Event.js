566865dc7e24da31a871212f8a68228102d72a7f
PanyushkinD <PanyushkinD@quantumart.ru>
2018-03-22 17:14:05 +0300

    Improve typed event

--- siteMvc/Scripts/Quantumart/Utils/Event.js
+++ siteMvc/Scripts/Quantumart/Utils/Event.js
@@ -1,39 +1,10 @@
 /**
- * Factory that creates event function.
- * Which delegates it's execution to list of handlers.
- * @param {object} sender Sender object
- * @returns Event function
- */
-export const event = function (sender) {
-  const listeners = [];
-
-  const e = function (...args) {
-    listeners.slice().forEach(handler => {
-      handler(sender, ...args);
-    });
-  };
-
-  e.attach = handler => {
-    listeners.push(handler);
-    return e;
-  };
-
-  e.detach = handler => {
-    // eslint-disable-next-line no-bitwise
-    listeners.splice(listeners.indexOf(handler) >>> 0, 1);
-    return e;
-  };
-
-  return e;
-};
-
-/**
  * Factory that creates async event function.
  * Which delegates it's execution to list of handlers.
  * @param {object} sender Sender object
  * @returns Event function
  */
-export const asyncEvent = function (sender) {
+export const event = function (sender) {
   const listeners = [];
 
   const e = function (...args) {
