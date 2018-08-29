28db7d5144a64c7fdb11a5c6112d8bd5809e792c
Pavel Celut <CelutP@quantumart.ru>
2018-08-29 14:12:47 +0300

    Update nuget version

--- bll/Properties/AssemblyInfo.cs
+++ bll/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("QP8.WebMvc.NUnit.Tests")]
 
 [assembly: AssemblyVersion("2.8.0.0")]
-[assembly: AssemblyInformationalVersion("2.8.2.0")]
+[assembly: AssemblyInformationalVersion("2.8.3.0")]
