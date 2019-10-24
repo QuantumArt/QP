e3c52d77fbf439d3a80e98563465490d7b3b3fe0
Pavel Celut <CelutP@gmail.com>
2019-10-24 18:26:43 +0300

    Update schedule dates

--- dal/scripts/current.sql
+++ dal/scripts/current.sql
@@ -5127,5 +5127,38 @@ IF NOT EXISTS (SELECT * FROM sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[C
   ADD END_DATE DATETIME NULL
 GO
 
+exec qp_drop_existing 'get_schedule_date', 'IsScalarFunction'
+GO
+
+CREATE FUNCTION dbo.get_schedule_date(@dt int, @tm int) returns datetime
+AS BEGIN
+    declare @result datetime;
+    declare @year int, @month int, @day int;
+    declare @hour int, @minute int, @second int;
+
+    set @year = @dt / 100;
+    set @day = @dt % 100;
+    set @month = @year % 100;
+    set @year = @year / 100;
+
+    set @hour = @tm / 100;
+    set @second = @tm % 100;
+    set @minute = @hour % 100;
+    set @hour = @hour / 100;
+
+    set @result = DATETIMEFROMPARTS(@year, @month, @day, @hour, @minute, @second, 0);
+
+    return @result;
+end
+GO
+
+update CONTENT_ITEM_SCHEDULE
+SET
+    START_DATE = dbo.get_schedule_date(isnull(active_start_date, 0), active_start_time),
+    END_DATE = dbo.get_schedule_date(active_end_date, active_end_time)
+
+GO
+
+
 GO
 
