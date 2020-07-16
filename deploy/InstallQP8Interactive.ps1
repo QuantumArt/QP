5130317c861472b0ec87bf941c498f3a952f4829
Pavel Celut <CelutP@gmail.com>
2020-07-16 21:17:26 +0300

    Update interactive script

--- deploy/InstallQP8Interactive.ps1
+++ deploy/InstallQP8Interactive.ps1
@@ -14,10 +14,17 @@ function Read-ValueOrDefault([string] $message, [string] $defaultValue) {
 }
 
 function Read-IntValueOrDefault([string] $message, [int] $defaultValue) {
-  $intValue = 0;
-  $value = Read-Host "$message (default - $defaultValue)"
-  $intValue = if ([int32]::TryParse($value, [ref] $intValue)) { $intValue } else { $defaultValue }
-  return $intValue
+    $intValue = 0;
+    $value = Read-Host "$message (default - $defaultValue)"
+    $intValue = if ([int32]::TryParse($value, [ref] $intValue)) { $intValue } else { $defaultValue }
+    return $intValue
+}
+
+function Read-YesOrNo([string] $message) {
+    while (-not($a -match "^[yn]$")) {
+        $a = Read-Host "$message (y/n)?"
+    }
+    return ($a.ToLower() -eq "y")
 }
 
 $name = Read-ValueOrDefault "Please enter site name to install" "QP8"
@@ -34,9 +41,14 @@ if (!$configFile) {
 
 $tempDir = Read-ValueOrDefault  "Please enter directory for temp files" "C:\Temp"
 $logDir = Read-ValueOrDefault  "Please enter directory for log files" "C:\Logs"
+$makeGlobal = Read-YesOrNo "Install globally"
+$useWinAuth = Read-YesOrNo "Use windows authentication"
+$enableArticleScheduler = Read-YesOrNo "Enable article scheduler"
+$enableCommonScheduler = Read-YesOrNo "Enable common scheduler"
 
 $currentPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $scriptName = Join-Path $currentPath "InstallQP8.ps1"
-Invoke-Expression "$scriptName -name $name -port $port -configUrl '$configUrl' -configToken '$configToken' -configDir '$configDir' -tempDir '$tempDir' -logDir '$logDir'"
+"$scriptName -name $name -port $port -configUrl '$configUrl' -configToken '$configToken' -configDir '$configDir' -tempDir '$tempDir' -logDir '$logDir' ``
+-makeGlobal `$$makeGlobal -useWinAuth `$$useWinAuth -enableArticleScheduler `$$enableArticleScheduler -enableCommonScheduler `$$enableCommonScheduler "
 
 
