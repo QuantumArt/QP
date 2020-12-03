58d19a3a1492d69e5c692a2a6b13b705abf15ca9
Pavel Celut <CelutP@gmail.com>
2020-12-03 20:44:05 +0300

    Build current.sql

--- dal/sql_scripts/current.sql
+++ dal/sql_scripts/current.sql
@@ -4494,17 +4494,29 @@ begin
 
 end
 
-update content_data set O2M_DATA = try_convert(numeric, cd.data) from content_data cd
-inner join content_attribute ca on ca.attribute_id = cd.attribute_id
-where ca.attribute_type_id = 11 and ca.link_id is null
-and cd.data is not null and isnumeric(cd.data) = 1 and cd.data <> '0' and cd.O2M_DATA is null
+declare @cnt numeric
+
+;with numbers(num)  as
+(
+	select top (1000) ROW_NUMBER() OVER (ORDER BY content_data_id) as num from content_data cd where cd.O2M_DATA is not null
+)
+select @cnt = max(num) from numbers
+
+
+if @cnt < 1000
+begin
+    update content_data set O2M_DATA = try_convert(numeric, cd.data) from content_data cd
+    inner join content_attribute ca on ca.attribute_id = cd.attribute_id
+    where ca.attribute_type_id = 11 and ca.link_id is null
+    and cd.data is not null and isnumeric(cd.data) = 1 and cd.data <> '0' and cd.O2M_DATA is null
+
+    update version_content_data set O2M_DATA = try_convert(numeric, vcd.data) from version_content_data vcd
+    inner join content_attribute ca on ca.attribute_id = vcd.attribute_id
+    where ca.attribute_type_id = 11 and ca.link_id is null
+    and vcd.data is not null and isnumeric(vcd.data) = 1 and vcd.data <> '0' and vcd.O2M_DATA is null
+end
 GO
 
-update version_content_data set O2M_DATA = try_convert(numeric, vcd.data) from version_content_data vcd
-inner join content_attribute ca on ca.attribute_id = vcd.attribute_id
-where ca.attribute_type_id = 11 and ca.link_id is null
-and vcd.data is not null and isnumeric(vcd.data) = 1 and vcd.data <> '0' and vcd.O2M_DATA is null
-GO
 
 update CONTENT_DATA set SPLITTED = i.splitted
 from content_data cd inner join CONTENT_ITEM i on i.content_item_id = cd.CONTENT_ITEM_ID
@@ -4599,7 +4611,7 @@ begin
 end
 GO
 
-declare @articles_with_wrong_statuses TABLE (
+DECLARE @articles_with_wrong_statuses TABLE (
 Site_ID int,
 CONTENT_ID int,
 STATUS_TYPE_ID int,
@@ -4609,11 +4621,9 @@ CONTENT_ITEM_ID int
 INSERT INTO @articles_with_wrong_statuses
 	SELECT c.Site_ID, c.CONTENT_ID, ci.STATUS_TYPE_ID, ci.CONTENT_ITEM_ID FROM [dbo].[CONTENT_ITEM] ci
 	INNER JOIN [dbo].[CONTENT] c ON ci.CONTENT_ID = c.CONTENT_ID
-	WHERE  ci.STATUS_TYPE_ID NOT IN (
-					SELECT STATUS_TYPE_ID
-					FROM [dbo].[STATUS_TYPE] i_st
-					WHERE i_st.SITE_ID = c.SITE_ID
-					)
+	INNER JOIN [dbo].[STATUS_TYPE] st ON ci.STATUS_TYPE_ID = st.STATUS_TYPE_ID
+	WHERE st.SITE_ID <> c.SITE_ID
+
 IF EXISTS (SELECT * FROM @articles_with_wrong_statuses)
 BEGIN
 DECLARE @statuses_names TABLE (
