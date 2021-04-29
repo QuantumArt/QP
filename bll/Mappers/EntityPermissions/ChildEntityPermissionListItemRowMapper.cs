4c1ed60c0bde3c1b8a5aa9aa9576091d1117af13
Pavel Celut <CelutP@gmail.com>
2021-04-29 09:28:33 +0300

    Fix Change All #162725

--- bll/Mappers/EntityPermissions/ChildEntityPermissionListItemRowMapper.cs
+++ bll/Mappers/EntityPermissions/ChildEntityPermissionListItemRowMapper.cs
@@ -14,8 +14,7 @@ namespace Quantumart.QP8.BLL.Mappers.EntityPermissions
                 .ForMember(biz => biz.Id, opt => opt.MapFrom(row => Converter.ToInt32(row.Field<decimal>("ID"))))
                 .ForMember(biz => biz.IsExplicit, opt => opt.MapFrom(row => row.Field<bool>("IsExplicit")))
                 .ForMember(biz => biz.Hide, opt => opt.MapFrom(row => row.Field<bool>("Hide")))
-                .ForMember(biz => biz.PropagateToItems, opt => opt.MapFrom(row => row.Field<bool>("PropagateToItems")))
-                .ForMember(biz => biz.Title, opt => opt.MapFrom(row => row.Field<string>("Title")))
+                .ForMember(biz => biz.PropagateToItems, opt => opt.MapFrom(row => Converter.ToBoolean(row.Field<decimal>("PropagateToItems"))))                .ForMember(biz => biz.Title, opt => opt.MapFrom(row => row.Field<string>("Title")))
                 .ForMember(biz => biz.LevelName, opt =>
                     opt.MapFrom(row => Translator.Translate(row.Field<string>("LevelName")) ?? EntityPermissionStrings.UndefinedPermissionLevel)
                 );
