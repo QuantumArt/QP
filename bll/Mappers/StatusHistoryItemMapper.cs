79cd16c7fb542fa9b7e2be2772f50c62152d7c66
Pavel Celut <CelutP@gmail.com>
2021-07-17 13:50:04 +0300

    Fix status list

--- bll/Mappers/StatusHistoryItemMapper.cs
+++ bll/Mappers/StatusHistoryItemMapper.cs
@@ -16,6 +16,7 @@ namespace Quantumart.QP8.BLL.Mappers
                 .ForMember(biz => biz.ActionMadeBy, opt => opt.MapFrom(row => Converter.ToString(row.Field<string>("ActionMadeBy"))))
                 .ForMember(biz => biz.ActionDate, opt => opt.MapFrom(row => Converter.ToDateTime(row.Field<DateTime>("ActionDate"))))
                 .ForMember(biz => biz.SystemStatusTypeName, opt => opt.MapFrom(row => Converter.ToString(row.Field<string>("SystemStatusTypeName"))))
+                .ForMember(biz => biz.Version, opt => opt.MapFrom(row => Converter.ToString(row.Field<decimal?>("Version"))))
                 .ForMember(biz => biz.StatusTypeName, opt => opt.MapFrom(row => Converter.ToString(row.Field<string>("StatusTypeName"))));
         }
     }
