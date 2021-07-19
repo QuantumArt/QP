a9d4e340474049f158f415cc5a2d34d70ad7fa80
Pavel Celut <CelutP@gmail.com>
2021-07-19 13:07:35 +0300

    Optimization

--- dal/sql_scripts/updates/content_item_version.sql
+++ dal/sql_scripts/updates/content_item_version.sql
@@ -1,18 +1,21 @@
 declare @hist1 table(version_id numeric primary key, status_type_id numeric, archive bit, visible bit)
 declare @hist2 table(version_id numeric primary key, status_type_id numeric, archive bit, visible bit)
 
+create table #status_history (num int, item_id decimal, version_id decimal, status_type_id int, visible bit, archive bit)
+create clustered index ix_temp_status_history ON #status_history (item_id, num)
+create nonclustered index ix_temp_version ON #status_history (version_id)
 
-;with history(num, id, item_id, version_id, status_type_id, visible, archive) as (
-    select row_number() over(partition by CONTENT_ITEM_ID order by status_history_id desc) as num,
-           status_history_id, content_item_id,  content_item_version_id, status_type_id, visible, archive
-    from content_item_status_history with(nolock)
-)
+insert into #status_history
+select row_number() over(partition by CONTENT_ITEM_ID order by status_history_id desc) as num,
+content_item_id,  content_item_version_id, status_type_id, visible, archive
+from content_item_status_history with(nolock) where datediff(month, CREATED, getdate()) <= 12
+
 insert into @hist1
-select h.version_id, h1.status_type_id, h1.archive, h1.visible from history h
-inner join history h1 on h.num = h1.num - 1 and h.item_id = h1.item_id
+select h.version_id, h1.status_type_id, h1.archive, h1.visible
+from #status_history h
+inner join #status_history h1 on h.item_id = h1.item_id and h.num = h1.num - 1
 inner join content_item_version v with(nolock) on h.version_id = v.content_item_version_id
-where v.STATUS_TYPE_ID is null and h1.status_type_id is not null
-order by h.id desc
+where h1.status_type_id is not null
 
 while exists(select * from @hist1)
 begin
@@ -23,4 +26,6 @@ begin
     set STATUS_TYPE_ID = h.status_type_id, VISIBLE = h.visible, ARCHIVE = h.archive
     FROM CONTENT_ITEM_VERSION v INNER JOIN @hist2 h ON v.content_item_version_id = h.version_id
 end
+
+drop table #status_history
 GO
