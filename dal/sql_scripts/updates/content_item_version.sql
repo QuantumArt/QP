b44a2d2a01a2265d21862dd0d954898ea1f881c7
Pavel Celut <CelutP@gmail.com>
2021-07-19 09:47:06 +0300

    Fix repeats

--- dal/sql_scripts/updates/content_item_version.sql
+++ dal/sql_scripts/updates/content_item_version.sql
@@ -1,4 +1,3 @@
-
 declare @hist1 table(version_id numeric primary key, status_type_id numeric, archive bit, visible bit)
 declare @hist2 table(version_id numeric primary key, status_type_id numeric, archive bit, visible bit)
 
@@ -6,21 +5,22 @@ declare @hist2 table(version_id numeric primary key, status_type_id numeric, arc
 ;with history(num, id, item_id, version_id, status_type_id, visible, archive) as (
     select row_number() over(partition by CONTENT_ITEM_ID order by status_history_id desc) as num,
            status_history_id, content_item_id,  content_item_version_id, status_type_id, visible, archive
-    from content_item_status_history where coalesce(system_status_type_id, 2) = 2
+    from content_item_status_history with(nolock)
 )
 insert into @hist1
 select h.version_id, h1.status_type_id, h1.archive, h1.visible from history h
 inner join history h1 on h.num = h1.num - 1 and h.item_id = h1.item_id
-where h.version_id is not null order by h.id desc
+inner join content_item_version v with(nolock) on h.version_id = v.content_item_version_id
+where v.STATUS_TYPE_ID is null and h1.status_type_id is not null
+order by h.id desc
 
 while exists(select * from @hist1)
 begin
     delete top(100) from @hist1
     output deleted.* into @hist2
 
-    update CONTENT_ITEM_VERSION
+    update CONTENT_ITEM_VERSION with(rowlock)
     set STATUS_TYPE_ID = h.status_type_id, VISIBLE = h.visible, ARCHIVE = h.archive
     FROM CONTENT_ITEM_VERSION v INNER JOIN @hist2 h ON v.content_item_version_id = h.version_id
-    Where v.STATUS_TYPE_ID is null
 end
 GO
