e1e669ffe8bb4f8d744ff79532e1504380adc905
Pavel Celut <CelutP@gmail.com>
2022-09-26 14:45:07 +0300

    Fix PR comments

--- QuantumArt.Schedulers/Quantumart.QP8.ArticleScheduler/ArticleSchedulerProperties.cs
+++ QuantumArt.Schedulers/Quantumart.QP8.ArticleScheduler/ArticleSchedulerProperties.cs
@@ -5,13 +5,16 @@ namespace Quantumart.QP8.ArticleScheduler
 {
     public class ArticleSchedulerProperties
     {
+        private const int DefaultRecurrentTimeout = 1;
+        private const int DefaultPrtgLoggerTasksQueueCheckShiftTime = 3;
+
         public ArticleSchedulerProperties()
         {
-            RecurrentTimeout = TimeSpan.FromMinutes(1);
-            PrtgLoggerTasksQueueCheckShiftTime = TimeSpan.FromMinutes(3);
+            RecurrentTimeout = TimeSpan.FromMinutes(DefaultRecurrentTimeout);
+            PrtgLoggerTasksQueueCheckShiftTime = TimeSpan.FromMinutes(DefaultPrtgLoggerTasksQueueCheckShiftTime);
         }
 
-        public ArticleSchedulerProperties(QPublishingOptions options)
+        public ArticleSchedulerProperties(QPublishingOptions options) : this()
         {
             XmlConfigPath = options.QpConfigPath;
             ConfigServiceUrl = options.QpConfigUrl;
