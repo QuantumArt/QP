b8261cdf191a4c9c0c6476b6a05eed6e546a28fc
Pavel Celut <CelutP@gmail.com>
2022-10-07 16:37:26 +0300

    Add host placeholder

--- move-qp-image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker pull '||registry-source||/qp:||version||' && \
-docker tag '||registry-source||/qp:||version||' '||registry-destination||/qp:||version||' && \
-docker push '||registry-destination||/qp:||version||'
