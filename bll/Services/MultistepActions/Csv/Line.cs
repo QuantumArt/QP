5e02ff523762f11aff1d6a7cb686ef5872670eef
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2022-11-18 13:33:05 +0300

    Replaced binary with custom csv

---
+++ bll/Services/MultistepActions/Csv/Line.cs
@@ -0,0 +1,11 @@
+namespace Quantumart.QP8.BLL.Services.MultistepActions.Csv
+{
+    public class Line
+    {
+        public int Number { get; set; }
+
+        public string Value { get; set; }
+
+        public bool Skip { get; set; }
+    }
+}
