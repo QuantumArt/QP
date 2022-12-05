dadf1182c707a7f8ae8172e51e482b2033af9042
Dakhraui Mokhamed <dakhrauim@qantumart.ru>
2022-12-05 15:53:45 +0300

    Add dummy mapper
    To search articles without loading fields metadata

---
+++ bll/Services/API/ExactMapper.cs
@@ -0,0 +1,10 @@
+﻿using Quantumart.QP8.BLL.Repository.ArticleMatching.Conditions;
+using Quantumart.QP8.BLL.Repository.ArticleMatching.Mappers;
+
+namespace Quantumart.QP8.BLL.Services.API
+{
+    public class ExactMapper : IConditionMapper<ConditionBase>
+    {
+        public ConditionBase Map(ConditionBase source) => source;
+    }
+}
