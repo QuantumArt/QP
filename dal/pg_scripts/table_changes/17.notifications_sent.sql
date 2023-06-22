3540663bc21e677e437253a531f57d99a50f575c
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2023-06-22 13:04:07 +0300

    Move seauqnce creation to appropriate place

--- dal/pg_scripts/table_changes/17.notifications_sent.sql
+++ dal/pg_scripts/table_changes/17.notifications_sent.sql
@@ -1,5 +1,3 @@
-CREATE SEQUENCE IF NOT EXISTS public.notifications_sent_seq START 1;
-
 ALTER TABLE public.notifications_sent ADD COLUMN IF NOT EXISTS id int NOT NULL DEFAULT nextval('notifications_sent_seq'::regclass);
 
 ALTER TABLE public.notifications_sent DROP CONSTRAINT IF EXISTS pk_notifications_sent;
