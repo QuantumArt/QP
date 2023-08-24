1577032287e9e2210e23315c06b8f8df5a56f05f
Pavel Celut <CelutP@gmail.com>
2023-08-24 17:21:49 +0300

    Fix link ownership

--- dal/pg_scripts/main/qp_change_contents_ownership.sql
+++ dal/pg_scripts/main/qp_change_contents_ownership.sql
@@ -6,16 +6,24 @@ AS $BODY$
     BEGIN
         for myrow in
         select 'ALTER TABLE '|| tablename ||' OWNER TO "'|| username ||'";' as tableq
-        from (select tablename from pg_catalog.pg_tables where tablename SIMILAR TO 'content_[\d]+[\w_]*') t
+        from (select tablename from pg_catalog.pg_tables where 
+			  tablename SIMILAR TO 'item_link_[\d]+[\w_]*'
+			  OR tablename SIMILAR TO 'content_[\d]+[\w_]*'
+			 ) t
         loop
             execute myrow.tableq;
         end loop;
-        
+
         for myrow in
         select 'ALTER VIEW '|| viewname ||' OWNER TO "'|| username ||'";' as viewq
-        from (select viewname from pg_catalog.pg_views where viewname SIMILAR TO 'content_[\d]+[\w_]+') v
+        from (select viewname from pg_catalog.pg_views where 
+			  viewname SIMILAR TO 'item_link_[\d]+[\w_]+' 
+			  OR viewname SIMILAR TO 'link_[\d]+[\w_]+'
+			  OR viewname SIMILAR TO 'content_[\d]+[\w_]+'
+			 ) v
         loop
             execute myrow.viewq;
         end loop;
+        
     END;
 $BODY$;
