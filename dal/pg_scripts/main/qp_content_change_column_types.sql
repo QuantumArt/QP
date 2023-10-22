89d7d384c74b06f255ccc62521dada5f8dcae91a
Pavel Celut <CelutP@gmail.com>
2023-10-22 12:04:17 +0300

    Fix scripts

--- dal/pg_scripts/main/qp_content_change_column_types.sql
+++ dal/pg_scripts/main/qp_content_change_column_types.sql
@@ -3,12 +3,10 @@ LANGUAGE 'plpgsql'
 AS $BODY$
     BEGIN
         call qp_content_new_views_drop(cid);
-        call qp_content_frontend_views_drop(cid);
         call qp_content_united_view_drop(cid);
         call qp_content_table_change_column_types(cid, true);
         call qp_content_table_change_column_types(cid, false);
         call qp_content_united_view_create(cid);
         call qp_content_new_views_create(cid);
-        call qp_content_frontend_views_create(cid);
     END;
 $BODY$;
