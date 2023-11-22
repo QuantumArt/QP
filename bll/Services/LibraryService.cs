36754a8255dad22308ba8accf0f471282cb4632b
Pavel Celut <CelutP@gmail.com>
2023-11-22 17:41:41 +0300

    Force using http setting

--- bll/Services/LibraryService.cs
+++ bll/Services/LibraryService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Quantumart.QP8.Configuration;
 
 namespace Quantumart.QP8.BLL.Services;
 
@@ -8,16 +9,19 @@ public class LibraryService : ILibraryService
 {
     private readonly IHttpClientFactory _factory;
     private readonly string _reduceSettingsEndpoint = "_reduce_settings";
+    private readonly QPublishingOptions _options;
 
-    public LibraryService(IHttpClientFactory factory)
+    public LibraryService(IHttpClientFactory factory, QPublishingOptions options)
     {
         _factory = factory;
+        _options = options;
     }
 
     public async Task<StorageReduceSettings> GetSettingsFromStorage(string url)
     {
         var client = _factory.CreateClient();
-        var result = await client.GetAsync($"{url}/{_reduceSettingsEndpoint}");
+        var resultUrl = _options.ForceHttpForImageResizing ? url.Replace("https:", "http:") : url;
+        var result = await client.GetAsync($"{resultUrl}/{_reduceSettingsEndpoint}");
         return result.IsSuccessStatusCode ? await result.Content.ReadFromJsonAsync<StorageReduceSettings>() : null;
     }
 }
