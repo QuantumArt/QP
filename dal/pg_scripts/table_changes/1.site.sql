418986076dce7dce8f71bf7bd8475c4f66ddd1cf
Pavel Celut <CelutP@gmail.com>
2024-01-31 10:28:24 +0300

    Add fields

--- dal/pg_scripts/table_changes/1.site.sql
+++ dal/pg_scripts/table_changes/1.site.sql
@@ -1,1 +1,2 @@
 ALTER TABLE site ADD COLUMN IF NOT EXISTS replace_urls_in_db boolean NOT NULL DEFAULT false;
+ALTER TABLE site ADD COLUMN IF NOT EXISTS use_native_ef_types boolean NOT NULL DEFAULT false;
