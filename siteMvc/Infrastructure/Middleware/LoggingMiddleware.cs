fa4eb6747e62191672f51099e1f3e0e2c1f2ba11
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2024-04-15 17:42:22 +0300

    Add extended logging & update nlog

---
+++ siteMvc/Infrastructure/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Quantumart.QP8.BLL;
+using Quantumart.QP8.WebMvc.Infrastructure.Constants;
+
+namespace Quantumart.QP8.WebMvc.Infrastructure.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            using IDisposable logScope = NLog.ScopeContext.PushProperties(new Dictionary<string, object>()
+            {
+                { LoggingAttributeConstants.UserIdAttribute, QPContext.CurrentUserId },
+                { LoggingAttributeConstants.UserNameAttribute, QPContext.CurrentUserName },
+                { LoggingAttributeConstants.UserIpAttribute, QPContext.GetUserIpAddress() },
+                { LoggingAttributeConstants.UserGroupIdsAttribute, string.Join(",", QPContext.CurrentGroupIds) }
+            });
+
+            await _next.Invoke(context);
+        }
+    }
+}
