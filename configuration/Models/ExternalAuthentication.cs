59ec3f63e87256ab477e7a1b357afbeb7c95e55e
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2025-03-27 11:41:42 +0300

    Login page settings

--- configuration/Models/ExternalAuthentication.cs
+++ configuration/Models/ExternalAuthentication.cs
@@ -7,4 +7,13 @@ public class ExternalAuthentication
     public bool Enabled { get; set; }
     public ExternalAuthenticationType ExternalAuthenticationType { get; set; } = ExternalAuthenticationType.None;
     public bool DisableInternalAccounts { get; set; }
+
+    public bool AllowLoginWithCredentials => !Enabled
+       || (Enabled
+           && (!DisableInternalAccounts
+               || ExternalAuthenticationType == ExternalAuthenticationType.ActiveDirectory));
+
+    public bool ShowSsoButton => Enabled && ExternalAuthenticationType == ExternalAuthenticationType.KeyCloak;
+
+    public bool ShowAutoLoginMessage => Enabled && ExternalAuthenticationType == ExternalAuthenticationType.ActiveDirectory;
 }
